7bab0dcde6e6e63af56826e67f45901bbd7c03f8
chandramanikumar <k.chandramani5265@gmail.com>
2025-05-03 20:01:10 +0530

    Mani | Sliding window log Rate Limiting Implemented

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import algo.impl.FixedWindowCounter;
 import algo.impl.LeakBucket;
+import algo.impl.SlidingWindowLog;
 import algo.impl.TokenBucket;
 
 public class Main {
@@ -7,9 +8,10 @@ public class Main {
 
 //        TokenBucket tokenBucket = new TokenBucket(5, 1);
 //        LeakBucket leakBucket = new LeakBucket(5, 1);
-        FixedWindowCounter fixedWindowCounter = new FixedWindowCounter(10, 1000);
+//        FixedWindowCounter fixedWindowCounter = new FixedWindowCounter(10, 1000);
+        SlidingWindowLog slidingWindowLog = new SlidingWindowLog(10, 1000);
         for (int i = 0; i < 100; i++) {
-            boolean allowed = fixedWindowCounter.tryAcquire();
+            boolean allowed = slidingWindowLog.tryAcquire();
             System.out.println("Request " + i + ": " + (allowed ? "Allowed" : "Denied"));
             Thread.sleep(50); // Simulate time between requests
         }
